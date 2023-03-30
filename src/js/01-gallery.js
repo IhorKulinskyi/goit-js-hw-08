262829a7fca7efcf6bd79daa75b022119c2ad5f9
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-30 15:04:10 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+galleryList.innerHTML = makeGalleryItemsMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function makeGalleryItemsMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li> `;
+    })
+    .join('');
+
+  return markup;
+}
