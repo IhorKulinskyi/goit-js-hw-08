a54a56923bd9ee901e816d07de68ba81c516cba8
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-31 13:11:55 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const formInputRef = document.querySelector('.feedback-form input');
+const formTextareaRef = document.querySelector('.feedback-form textarea');
+
+onPopulateFormData();
+
+formRef.addEventListener('input', throttle(onInputForm, 500));
+formRef.addEventListener('submit', onSubmitForm);
+
+function onInputForm(e) {
+  const formDataStr = JSON.stringify({
+    email: formInputRef.value,
+    message: formTextareaRef.value,
+  });
+  localStorage.setItem('feedback-form-state', formDataStr);
+}
+
+function onPopulateFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+
+  if (savedData) {
+    formInputRef.value = JSON.parse(savedData).email;
+    formTextareaRef.value = JSON.parse(savedData).message;
+  }
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  if (formInputRef.value === '' || formTextareaRef.value === '') {
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  e.currentTarget.reset();
+}
